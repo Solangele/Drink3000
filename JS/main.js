52c345ce56e642dd365f9b2e75a909ddf137d80b
Solangèle <angedespretz@gmail.com>
2023-12-18 15:04:50 +0100

    emoji

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,9 @@
+let cocktail = [
+    "Original",
+    "Mojito",
+    "Tropical"  
+];
+
 let original = document.querySelector(".selectionnerOr");
 let mojito = document.querySelector(".selectionnerMo");
 let tropical = document.querySelector(".selectionnerTro");
@@ -6,4 +12,30 @@ let boisson = document.querySelector(".nomBoisson");
 
 let choix = "Aucune";
 //utilisation innertext pour affecter la valeur à .nomBoisson
+choix = boisson.innerText;
+
+original.addEventListener("click", () => {
+    choix = cocktail[0];
+    boisson.textContent = cocktail[0];
+    console.log(choix);
+});
+
+mojito.addEventListener("click", () => {
+    choix = cocktail[1];
+    boisson.textContent = cocktail[1];
+    console.log(choix);
+});
+
+tropical.addEventListener("click", () => {
+    // choix = cocktail[2];
+    boisson.textContent = cocktail[2];
+    // console.log(choix);
+});
+
+
+let energie = document.querySelector(".btnEnergie");
+let desalt = document.querySelector(".btnDesalt");
+
+
+energie.addEventListener();
 
